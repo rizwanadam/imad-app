82f419cf614236624c6b749d3902cdd7da21304d
rizwanadam <riam16cs@cmrit.ac.in>
2018-02-20 20:05:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,6 +57,10 @@ app.get('/article-one', function(req,res)
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }
 )
+app.get('/ui/main.js',function(req,res)
+{
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+})
 app.get('/article-two', function(req,res)
 {
    res.send(createTemplate(articleTwo));
