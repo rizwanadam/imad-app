ef4ecc4b8ad0f4c663118f8f7c597d1f71d3f6c4
rizwanadam <riam16cs@cmrit.ac.in>
2018-02-20 20:18:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 
 var element = document.getElementById("main-text");
 element.innerHTML = "This is a modification through javascript";
+var img = document.getElementById("madi");
+img.onclick = function(){
+    img.style.marginLeft = '100px'
+}
