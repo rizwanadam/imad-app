19c13a34028d5759764db709e59aba726ddf397e
rizwanadam <riam16cs@cmrit.ac.in>
2018-02-20 22:38:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,7 @@
 var button = document.getElementById('counter');
 button.onclick = function()
 {
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function()
-    {
-        if(request.readyState === XMLHttpRequest.DONE)
-        {
-            if(request.status === 200)
-            {
-                var counter = request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET','http://rizwanadam45.imad.hasura-app.io/counter',true)
-    request.send(null);
+   counter++;
+   var span = document.getElementById('count');
+   span.innerHTML = counter.toString();
 };
