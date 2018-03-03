5db243a195bfe9ae97740151fc61afb322c20b07
rizwanadam <riam16cs@cmrit.ac.in>
2018-03-03 23:25:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,8 @@ app.get('/article-one', function(req,res)
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }
 )
+
+var pool = new Pool(config);
 app.get('/test-db',function(req,res)
 {
     pool.query('SELECT * FROM test',function(err,result){
