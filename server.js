3f75dc3cb091725dad65feb00f2ce33183fe073b
rizwanadam <riam16cs@cmrit.ac.in>
2018-03-03 23:25:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/test-db',function(req,res)
     pool.query('SELECT * FROM test',function(err,result){
         if(err)
         {
-            res.status(500).send(err.toString())
+            res.status(500).send(err.toString());
         }
         else
         {
