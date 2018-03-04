9172c777aa375d5be644e11f60ea95ae62ea4f23
rizwanadam <riam16cs@cmrit.ac.in>
2018-03-04 11:58:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/article-one', function(req,res)
 var pool = new Pool(config);
 app.get('/articles/articleName',function(req,res)
 {
-    pool.query("SELECT * FROM article EHERE title = '" + req.params.articleName+"'",function(err,result)
+    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName+"'",function(err,result)
     {
         if(err)
         {
