b78d7b541f6d656cc0f65485178ffa232990897e
rizwanadam <riam16cs@cmrit.ac.in>
2018-03-06 20:51:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ submit.onclick = function()
     var password = document.getElementById("password").value;
     console.log('username');
     console.log('password');
-    request.open('POST','http://rizwanadam45.imad.hasura-app.io/counter',true);
+    request.open('POST','http://rizwanadam45.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username: username ,password: password}));
 };
