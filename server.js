bfebe5e224d957e71797be2d189c0716aa717227
rizwanadam <riam16cs@cmrit.ac.in>
2018-03-06 20:02:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post('/login',function(req,res)
                  }
                  else
                  {
-                     res.send(403).send('username/password is invalid!');
+                     res.status(403).send('username/password is invalid!');
                  }
             }
         }
